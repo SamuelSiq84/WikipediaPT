690f83322f8d548e0c33179e62c1d8e3136f060d
SamuelSiq84 <samuel.siqueira.pereira@gmail.com>
2021-04-02 18:21:02 -0300

    Alteração geral do script. comentado no arquivo runner java dryrun e minichrome

--- src/test/java/steps/Post.java
+++ src/test/java/steps/Post.java
@@ -1,6 +1,9 @@
 package steps;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
@@ -18,20 +21,20 @@ public class Post extends Base{
     }
 
 
-    @Dado("^quando acesso a Wikipedia em Portugues$")
+    @Given("^quando acesso a Wikipedia em Portugues$")
     public void quandoAcessoAWikipediaEmPortugues() {
         base.driver.get (base.url); // abre o navegador no site alvo ( extendendo da base)
         System.out.println("Passo 1 - Acessou a página");
 
     }
 
-    @Quando("^pesquiso por \"([^\"]*)\"$")
+    @When("^pesquiso por \"([^\"]*)\"$")
     public void pesquisoPor(String pascoa)  {
         base.driver.findElement(By.id("searchInput")).sendKeys(pascoa + Keys.ENTER);
         System.out.println("Passo 2 - Pesquisou por " + pascoa );
     }
 
-    @Entao("^Exibe a expressao \"([^\"]*)\" no titulo da guia$")
+    @Then("^Exibe a expressao \"([^\"]*)\" no titulo da guia$")
     public void exibeAExpressaoNoTituloDaGuia(String pascoa)  {
         //assertEquals("Ovo de Páscoa – Wikipédia, a enciclopédia livre",base.driver.getTitle());
         assertTrue(base.driver.getTitle().contains(pascoa));
--- src/test/resources/features/Post.feature
+++ src/test/resources/features/Post.feature
@@ -1,7 +1,14 @@
-#undecoding: utf-8
-#language: pt
- Funcionalidade:  Consulta
-   Cenario: Consultar Ovo de Pascoa
-     Dado quando acesso a Wikipedia em Portugues
-     Quando pesquiso por "Ovo de PÃ¡scoa"
-     Entao Exibe a expressao "Ovo de PÃ¡scoa" no titulo da guia
+
+# Funcionalidade:  Consulta
+  # Cenario: Consultar Ovo de Pascoa
+     #Dado quando acesso a Wikipedia em Portugues
+     #Quando pesquiso por "Ovo de Páscoa"
+     #Entao Exibe a expressao "Ovo de Páscoa" no titulo da guia
+
+
+
+Feature: Consulta
+  Scenario: Consultar Ovo de Pascoa
+    Given quando acesso a Wikipedia em Portugues
+    When pesquiso por "Ovo de Páscoa"
+    Then Exibe a expressao "Ovo de Páscoa" no titulo da guia
